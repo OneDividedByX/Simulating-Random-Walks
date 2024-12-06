c67e07f0fbb3ec697e68523dd529d23a568fa387
dx <jael.laiza.g@uni.pe>
2024-12-06 11:25:02 -0500

    Add files via upload

--- Simulated Random Walks/main.py
+++ Simulated Random Walks/main.py
@@ -18,10 +18,10 @@ from experimental.random_walks.CuponCollector import simulate_CuponCollector_Hit
 ################################################################################
 ################################################################################
 # Define the transition matrix
-v_0=np.array([0.1,0.3,0.5,0.1])
-v_1=np.array([0,0.5,0.5,0])
-v_2=np.array([0.5,0.2,0.2,0.1])
-v_3=np.array([0.5,0,0,0.5])
+v_0=np.array([0.0,0.5,0,0.5])
+v_1=np.array([0.5,0,0.5,0])
+v_2=np.array([0,0.5,0,0.5])
+v_3=np.array([0.5,0,0.5,0])
 P=np.array([v_0,v_1,v_2,v_3])
 X_0=0; n_steps=1000
 
@@ -30,15 +30,14 @@ prob_coin =np.array([0.5,0.5])
 
 # simulate_GamblersRuin(8,5,prob_coin,speed=1,n_steps='stopped')
 # graph_simulate_GamblersRuin(15,7,prob_coin,speed=10,state='discrete',n_steps=None)
-# simulate_markov_chain(P, X_0, speed=10, n_steps=None)
-
-# graph_simulate_markov_chain(P,X_0,5,'discrete',n_steps=None)
+simulate_markov_chain(P, X_0, speed=1, n_steps=15)
+# graph_simulate_markov_chain(P,X_0,5,'discrete',n_steps=100)
 
 ################################################################################
 ################################################################################
 XY_0=np.array([0,0]); speed=10
 # graph_RandomWalkInZxZ_1(XY_0,speed,'discrete',annotate=True)
-graph_RandomWalkInZxZ_2(XY_0,speed,'discrete',annotate=True)
+# graph_RandomWalkInZxZ_2(XY_0,speed,'discrete',annotate=True)
 
 ################################################################################
 ################################################################################
